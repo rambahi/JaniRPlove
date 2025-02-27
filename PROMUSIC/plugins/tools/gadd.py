fab716b9ad21302f8dbb2e85007d509a040b31aa
lajabajwkjsaiajajahjwkdhdywhjwjsjsgs <terabaaphume120@gmail.com>
2025-02-28 00:54:30 +0530

    Update gadd.py

--- PROMUSIC/plugins/tools/gadd.py
+++ PROMUSIC/plugins/tools/gadd.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "7520092354"
+OWNERS = "7995626429"
 from PROMUSIC import app
 from PROMUSIC.utils.database import add_served_chat, get_assistant
 
@@ -12,7 +12,7 @@ async def add_allbot(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
         await message.reply(
-            "**❍ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @MeowMusicXbot`**"
+            "**❍ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @AAROHI_MUSICBOT`**"
         )
         return
 
